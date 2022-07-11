c249c3bdd67bc7ff9c7de96dbf55a7de5c6d4b7c
Evan Comtesse <evancomtesse@Evans-MacBook-Pro.local>
2022-07-11 19:35:40 +1000

    set up models schema and inserts seed data to mongodb

---
+++ models/models.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const modelsSchema = new Schema (
+    {
+        name: {type: String, required: true},
+        brand: {type: String},
+        img: {type:String, default: "https://loremflickr.com/600/600/car"},
+        manufacturer: String,
+        model: String,
+        yearOfProduction: Number,
+        purchasePrice: {type: Number, min: 1},
+        currentValue: {type: Number, min: 1},
+        preparedtoSell: {type: Boolean, default: true},
+        soldPrice: {type: Number, min: 1},
+        qty: {type: Number, min: 1},
+        otherInfo: String
+    },
+    {timestamps: true}
+)
+
+const Models = mongoose.model("Models", modelsSchema)
+
+module.exports = Models
--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 // DEPENDENCIES
+// Requires the dotenv package to allow people/servers to run the code on different ports.
+require("dotenv").config()
+
+// Requires the express package to create a dynamic app.
 const express = require("express")
+// Requries this mongoose express package to communicate between the mongodb database, our server and the client.
+const mongoose = require("mongoose")
+// Package required to manipulate the database (edit, create, delete)
+const methodOverride = require("method-override")
+const { log } = require("console")
+//
+// const session = requrie("express-session")
 
 //CONFIGURATION
+// Calls the express function required above.
 const app = express()
-const PORT = 3000
+// Defines the PORT variable as per the local .env file.
+const PORT = process.env.PORT
+// Defines the URL for the mongodb database.
+const dbURL = "mongodb://localhost:27017/models"
+
+
+// Middleware
+// This uses the urleconded function in express to the URL sting into objects that can be used to query/manipulate data in the database.
+app.use(express.urlenocded({ extended: false }))
+// Calls the express static function to allow static files (e.g. css) to be run on dynamic pages.
+app.use(express.static("public"))
+// Calls the method function to allow us to use the create, edit and delete functions.
+app.use(methodOverride("_method"))
+
+
 
 // ROUTES
 // Index Route
@@ -17,6 +43,12 @@ app.get("/:id", (req, res) => {
 })
 
 // LISTENERS
+// Function to connect the server to the monogodb database.
+mongoose.connect(dbURL, () => {
+    log("Connected to models db")
+})
+
+// Listen function to tell the server to listen for requests on the defined port.
 app.listen(PORT, () => {
     console.log("Server running on port: ", PORT);
 })
