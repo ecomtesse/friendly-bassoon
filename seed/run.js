197ea327936004f46b9c71838e6beb34ecee61a9
Evan Comtesse <evancomtesse@Evans-MacBook-Pro.local>
2022-07-15 10:37:25 +1000

    changes to seed to heroku

---
+++ seed/run.js
@@ -0,0 +1,27 @@
+// Requires the npm mongoose package
+const mongoose = require("mongoose")
+
+// Requires the model schema
+const Models = require("../models/models.js")
+// Requuires the dummy dataset.
+const dummyModels = require("./dummy-models")
+
+// Defining the mongodb database url in a variable.
+const dbURL = "mongodb://localhost:27017/models"
+
+// Function to wipe any previous data in the collection and seed the dummy data to the mongodb database.
+mongoose.connect(dbURL, () => {
+    console.log("connected to models db")
+    // console.log("resetting models collection")
+    // Models.collection.drop()
+    //     .then(() => {
+    //         console.log("Models collection dropped")
+    //         console.log("inserting seed data")
+    //         return 
+    Models.insertMany(dummyModels)
+        // })
+        .then(() => {
+            console.log("inserted models seed data")
+            mongoose.connection.close();
+        })
+})
